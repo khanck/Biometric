101ae85462b397975e22a2d389802d931253b1e4
DESKTOP-2PJ7HIB\Muhammad Kamran <muhammad.kamrankhan@neom.com>
2024-10-16 13:38:24 +0300

    added logs

--- TCC.Biometric.Payment/Handlers/WebSocketHandler.cs
+++ TCC.Biometric.Payment/Handlers/WebSocketHandler.cs
@@ -5,6 +5,11 @@ namespace TCC.Biometric.Payment.Handlers
 {
     public class WebSocketHandler
     {
+        private readonly ILogger<WebSocketHandler> _logger;
+        public WebSocketHandler(ILogger<WebSocketHandler> logger)
+        {
+                _logger = logger;
+        }
         // Use ConcurrentDictionary to store connected clients
         private static ConcurrentDictionary<string, WebSocket> _clients = new ConcurrentDictionary<string, WebSocket>();
 
@@ -25,7 +30,9 @@ namespace TCC.Biometric.Payment.Handlers
                 // Add the new client to the dictionary
                 _clients.TryAdd(clientId, webSocket);
 
-                Console.WriteLine($"Client connected: {clientId}");
+                //Console.WriteLine($"Client connected: {clientId}");
+                _logger.Log(LogLevel.Information, $"Client connected: {clientId}");
+
 
                 // Handle incoming messages from the client (if necessary)
                 await ReceiveMessagesAsync(webSocket, clientId);
@@ -48,24 +55,30 @@ namespace TCC.Biometric.Payment.Handlers
             {
                 result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
                 var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
-                Console.WriteLine($"Received message from client {clientId}: {message}");
-               await BroadcastMessage(message);
+                //Console.WriteLine($"Received message from client {clientId}: {message}");
+                _logger.Log(LogLevel.Information, $"ReceiveMessagesAsync clientId: {clientId} Message : {message}");
+
+                await BroadcastMessage(message);
                 // Process the message if needed
 
             } while (!result.CloseStatus.HasValue);
 
             // Close the connection when done
             await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
-            Console.WriteLine($"Client disconnected: {clientId}");
+            //Console.WriteLine($"Client disconnected: {clientId}");
+            _logger.Log(LogLevel.Information, $"Client disconnected: {clientId}");
+
         }
 
         public async Task SendMessageToClient(string clientId, string message)
         {
+
            if (_clients.TryGetValue(clientId, out var webSocket) && webSocket.State == WebSocketState.Open)
             {
                 var messageBytes = Encoding.UTF8.GetBytes(message);
                 await webSocket.SendAsync(new ArraySegment<byte>(messageBytes), WebSocketMessageType.Text, true, CancellationToken.None);
-           }
+                _logger.Log(LogLevel.Information, $"SendMessageToClient clientId: {clientId} message {message}");
+            }
         }
 
         public async Task BroadcastMessage(string message)
@@ -77,6 +90,8 @@ namespace TCC.Biometric.Payment.Handlers
                 var webSocket = client.Value;
                 if (webSocket.State == WebSocketState.Open)
                 {
+                    _logger.Log(LogLevel.Information, $"BroadcastMessage  message {message}");
+
                     await webSocket.SendAsync(new ArraySegment<byte>(messageBytes), WebSocketMessageType.Text, true, CancellationToken.None);
                 }
             }
